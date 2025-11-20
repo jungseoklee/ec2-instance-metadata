54eea8be3379ef3c21d420a58d9dde6b7e57a1d8
Jungseok Lee <jungseoklee85@gmail.com>
2025-11-20 19:51:44 +0000

    Print out query results in a JSON format along with bug fixes
    This patch prints out query results in a JSON format along with bug fixes.
    
    Signed-off-by: Jungseok Lee <jungseoklee85@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 use chrono::Utc;
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use std::{
-    error::Error, sync::{Arc, RwLock}, thread, time::{Duration, Instant},
+    error::Error, sync::{Arc, OnceLock, RwLock}, thread, time::{Duration, Instant},
 };
 
 const ENDPOINT: &str = "http://169.254.169.254";
 const TOKEN_TTL: Duration = Duration::from_hours(6);
-const TOKEN_REFRESH_OFFSET: Duration = Duration::from_hours(1);
+const TOKEN_REFRESH_OFFSET: Duration = Duration::from_hours(2);
+
+static TIMESTAMP_FORMAT: OnceLock<TimestampFormat> = OnceLock::new();
 
 #[derive(Parser)]
-#[command(name = "ec2im")]
+#[command(name = "ec2im", about = "EC2 Instance Metadata CLI")]
 struct Cli {
     #[command(subcommand)]
     command: Command,
+    #[arg(long, short = 't', value_enum, default_value_t = TimestampFormat::Iso, global = true)]
+    timestamp_format: TimestampFormat,
 }
 
 #[derive(Subcommand)]
 enum Command {
+    #[command(about = "Get instance metadata once")]
     Get {
         path: String,
     },
+    #[command(about = "Get instance metadata periodically")]
     Poll {
         path: String,
-        #[arg(long, default_value_t = 5000, value_parser = clap::value_parser!(u64).range(100..=5000))]
+        #[arg(long, short = 'i', default_value_t = 5000, value_parser = clap::value_parser!(u64).range(500..=10000))]
         interval: u64,
     },
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum TimestampFormat {
+    Iso,
+    Unix,
 }
 
 fn get_token() -> Result<String, Box<dyn Error>> {
@@ -56,7 +68,7 @@ fn query(token: &str, path: &str) -> Result<String, Box<dyn Error>> {
     if output.status.success() {
         Ok(String::from_utf8(output.stdout)?.trim().to_string())
     } else {
-        Err("Failed to get data for {path}.".into())
+        Err(format!("Failed to get data for {path}.").into())
     }
 }
 
@@ -89,24 +101,34 @@ fn poll(init_token: String, path: &str, interval_ms: u64) -> Result<(), Box<dyn 
 
     loop {
         let current_token = token.read().unwrap().clone();
-        match query(&current_token, path) {
-            Ok(res) => println!("{}: {res}", Utc::now()),
-            Err(e) => eprintln!("{}: {e}", Utc::now()),
-        }
+        println!("{}", to_json(&path, query(&current_token, path)));
         thread::sleep(interval);
+    }
+}
+
+fn to_json(path: &str, res: Result<String, Box<dyn Error>>) -> String {
+    let timestamp = match TIMESTAMP_FORMAT.get().unwrap_or(&TimestampFormat::Iso) {
+        TimestampFormat::Iso => Utc::now().format("%Y-%m-%dT%H:%M:%S%.3f").to_string(),
+        TimestampFormat::Unix =>  Utc::now().timestamp_millis().to_string()
+    };
+    match res {
+        Ok(v) => format!(
+            r#"{{"timestamp": "{}", "path": "{}", "value": "{}", "status": "success"}}"#,
+            timestamp, path, v),
+        Err(e) => format!(
+            r#"{{"timestamp": "{}", "path": "{}", "value": null, "status": "error", "reason": "{}"}}"#,
+            timestamp, path, e),
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-    let token = get_token()?;
+    TIMESTAMP_FORMAT.set(cli.timestamp_format).unwrap();
 
+    let token = get_token()?;
     match cli.command {
         Command::Get { path } => {
-            match query(&token, &path) {
-                Ok(res) => println!("{}: {res}", Utc::now()),
-                Err(e) => eprintln!("{}: {e}", Utc::now()),
-            }
+            println!("{}", to_json(&path, query(&token, &path)))
         },
         Command::Poll { path, interval } => {
             poll(token, &path, interval)?;
